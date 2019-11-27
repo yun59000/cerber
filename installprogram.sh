9faf39144a6d3f7e0e05f1d10b9d344a12979c3e
Yves Richard <richard.yves-isc.france@ibm.com>
2019-11-27 08:04:15 +0100

    first final version V1.0

--- installprogram.sh
+++ installprogram.sh
@@ -3,48 +3,44 @@
 timestamp=`date "+%Y%m%d-%H%M%S"`
 
 
-echo "launch installprogramm"
+(echo "$timestamp launch installprogramm" 2>&1) >> "log.txt"
+tail -1 "log.txt"
+#retreive the user name who launch the sudo
+if [ $SUDO_USER ]; then 
+    user=$SUDO_USER
+else 
+    user=`whoami`
+fi
+
 installProgram () {
     if ! [ "$1" = "" ]; then
         #---------------------------
         PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $1|grep "install ok installed")
-        echo "Checking for somelib: $PKG_OK"
+        (echo "$timestamp Checking for somelib: $PKG_OK" 2>&1) >> "log.txt"
+        tail -1 "log.txt"
         if [ "" == "$PKG_OK" ]; then
-            echo "------------------INSTALL----------------------"
-            echo "$timestamp Installing package $1 ------------ "
-            echo "-------------------------------------------------"
+            (echo "------------------INSTALL----------------------" 2>&1) >> "log.txt"
+            (echo "$timestamp Installing package $1 ------------ " 2>&1) >> "log.txt"
+            (echo "-------------------------------------------------" 2>&1) >> "log.txt"
+            tail -3 "log.txt"
             if [ "$1" = "docker-machine" ]; then
                 base=https://github.com/docker/machine/releases/download/v0.14.0 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine
             else
                 apt-get --force-yes --yes install $1
             fi            
             if [ "" == "$PKG_OK" ]; then
-                echo "-------------------------------------------------"
-                echo "$timestamp package $1 installed Successfully--- "
-                echo "-------------------------------------------------"    
-            fi
-            
-        fi
-        #---------------------------
-        if ! which $1 > /dev/null; then
-            echo "------------------INSTALL----------------------"
-            echo "$timestamp Installing package $1 ------------ "
-            echo "-------------------------------------------------"
-            if [ "$1" = "docker-machine" ]; then
-                base=https://github.com/docker/machine/releases/download/v0.14.0 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine
-            else
-                apt-get install $1
-            fi            
-            if which $1 > /dev/null; then
-                echo "-------------------------------------------------"
-                echo "$timestamp package $1 installed Successfully--- "
-                echo "-------------------------------------------------"    
+                (echo "---------------------INSTALLED----------------------------" 2>&1) >> "log.txt"
+                (echo "$timestamp package $1 installed Successfully--- " 2>&1) >> "log.txt"
+                (echo "----------------------------------------------------------" 2>&1) >> "log.txt"
+                tail -3 "log.txt"
             fi
         else
-            echo "-------------------------------------------------"
-            echo "$timestamp package $1 already install ---Skipped "
-            echo "-------------------------------------------------"
+            (echo "----------------------------SKIPPED-----------------------" 2>&1) >> "log.txt"
+            (echo "$timestamp package $1 already install ---Skipped " 2>&1) >> "log.txt"
+            (echo "----------------------------------------------------------" 2>&1) >> "log.txt" 
+            tail -3 "log.txt"
         fi
+       
     fi
     
 }
@@ -53,3 +49,84 @@ installProgram "docker"
 #installProgram "docker-compose"
 #installProgram "curl"
 #installProgram "wget"
+
+#ajouter l'utilisateur courant au groupe docker
+    #recuperer l'utilisateur courant pas le root
+
+(echo "$timestamp user is : $user"  2>&1) >> "log.txt"
+#check if docker group exist:
+dockerInGroup=$(cat /etc/group |grep "docker")
+userInDockerGroup=$(cat /etc/group |grep "docker" |grep "$user")
+if [ "$dockerInGroup" = "" ]; then
+    (echo "$timestamp the docker group does not exist"  2>&1) >> "log.txt"
+    (echo "$timestamp docker group creation !"  2>&1) >> "log.txt"
+    tail -2 "log.txt"
+    groupadd docker
+else
+    (echo "$timestamp the docker group exist, no need to create" 2>&1) >> "log.txt"
+    tail -1 "log.txt"
+fi
+if ! [ "$dockerInGroup" = "" ] && [ "$userInDockerGroup" = "" ]; then
+    #check if user belong to docker group
+    (echo "$timestamp the user $user does not belong to the docker group"  2>&1) >> "log.txt"
+    (echo "$timestamp adding the user $user to the docker group"  2>&1) >> "log.txt"
+    tail -2 "log.txt"
+    usermod -aG docker $user
+    (echo "$timestamp test after usermod docker" 2>&1) >> "log.txt"
+    #adduser "$user" docker
+    #refreshing the group file
+    #newgrp "docker"
+    (echo "$timestamp test after newgrp" 2>&1) >> "log.txt"
+else
+    if [ "$(cat /etc/group |grep docker |grep $user )" ]; then
+        (echo "$timestamp the user already belong to the docker group")
+    else
+        (echo "$timestamp an err occured in group affectation")
+    fi
+    
+fi
+
+#creation of a cerberus folder on the desktop
+defaultIsDownloaded=false
+pathToYML="/home/$user/cerberus/"
+if ! [ -d "$pathToYML" ]; then
+    mkdir "$pathToYML"
+    cd "$pathToYML"
+
+    wget https://raw.githubusercontent.com/cerberustesting/cerberus-source/master/docker/compositions/cerberus-glassfish-mysql/default-with-selenium.yml
+    defaultIsDownloaded=true
+else
+    cd "$pathToYML"
+    if ! [ -f "default-with-selenium.yml" ];then
+        wget https://raw.githubusercontent.com/cerberustesting/cerberus-source/master/docker/compositions/cerberus-glassfish-mysql/default-with-selenium.yml
+    defaultIsDownloaded=true
+    fi    
+fi
+
+if [ "$defaultIsDownloaded" = "true" ];then
+    echo "on est $(pwd)"
+    if ! [ -f "default-with-selenium.yml" ];then
+        mv default-with-selenium.yml docker-compose.yml
+        (echo "$timestamp on créer le docker-compose a partir du default-selenium" 2>&1) >> "log.txt"
+        tail -1 "log.txt"
+    else
+        (echo "$timestamp what to do ?"  2>&1) >> "log.txt"
+        tail -1 "log.txt"
+    fi
+fi
+
+(echo "$timestamp ready for launch"  2>&1) >> "log.txt"
+(echo "ready ?" 2>&1) >> "log.txt"
+tail -2 "log.txt"
+read
+if [ "$REPLY" = "y" ]; then
+    (echo "$timestamp docker compose up" 2>&1) >> "log.txt"
+    tail -1 "log.txt"
+    #docker-compose up
+    # curl our verifier si le site est up
+else
+    (echo "$timestamp see ya !"  2>&1) >> "log.txt"
+    tail -1 "log.txt"
+fi
+
+#pb newgrp
--- main.sh
+++ main.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if ((${EUID:-0} || "$(id -u)")); then
-  echo "You must have root priviledge to launch this script"
+  (echo "You must have root priviledge to launch this script" 2>&1) >> "log.txt"
+  tail -1 "log.txt"
 else
-  echo "Hello, root."
+  (echo "-------------Cerberus-INSTALL------------------" 2>&1) >> "log.txt" 
+  tail -1 "log.txt"
   bash installprogram.sh
 fi
