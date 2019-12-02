7df7bed6c38fd17485f664e2f740f91c5437d050
Yves Richard <richard.yves-isc.france@ibm.com>
2019-12-02 18:06:43 +0100

    fix subshell for echo's

--- main.sh
+++ main.sh
@@ -5,10 +5,10 @@ timestamp=`date "+%Y%m%d-%H%M%S"`
 currentPath="$(pwd)"
 
 if ((${EUID:-0} || "$(id -u)")); then
-  (echo "${timestamp} You must have root priviledge to launch this script" 2>&1) >> "${currentPath}/log.txt"
+  echo "${timestamp} You must have root priviledge to launch this script" &>> "${currentPath}/log.txt"
   tail -1 "${currentPath}/log.txt"
 else
-  (echo "-------------------------Cerberus-INSTALL------------------" 2>&1) >> "${currentPath}/log.txt" 
+  echo "-------------------------Cerberus-INSTALL------------------" &>> "${currentPath}/log.txt" 
   tail -1 "${currentPath}/log.txt"
   bash installprogram.sh
 fi
