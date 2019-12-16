196bc675e28497b0ed33cc3671a627cb93cf1e79
Yves Richard <richard.yves-isc.france@ibm.com>
2019-12-16 14:19:37 +0100

    add the adress to connect to cerberus

--- installprogramV2.sh
+++ installprogramV2.sh
@@ -366,6 +366,7 @@ if [ "${launch}" = "yes" ]
        if [ "${access}" = "200" ]
             then
             echo "${timestamp} --------Cerberus is Up and Runnin---------" &>> "${currentPath}/log.txt"
+            echo "${timestamp} --------@: http://127.0.0.1:8080 ---------" &>> "${currentPath}/log.txt"            
             tail -1 "${currentPath}/log.txt"
             # echo "for test purposes in up ${nb_try} ---"
             nb_try=30
